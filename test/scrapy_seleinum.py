fe6e4dbce5bc4739ca1c83e232fc7d123d817473
longl <longl@ghsp.com>
2024-11-21 11:09:07 +0800

    通过登录个人公众号抓取

--- test/scrapy_seleinum.py
+++ test/scrapy_seleinum.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from icecream import ic
 import re
 import time
 from functools import reduce
@@ -12,71 +15,134 @@ note: 需要使用selenium，chrome版本需要与chromedriver版本对应。具
 """
 
 
-def login(username, password):
+def login(username='', password=''):
     # 打开微信公众号登录页面
     driver.get("https://mp.weixin.qq.com/")
     driver.maximize_window()
-    time.sleep(3)
+    # time.sleep(1)
     # 自动填充帐号密码
-    driver.find_element_by_xpath(
-        '//*[@id="header"]/div[2]/div/div/form/div[1]/div[1]/div/span/input'
-    ).clear()
-    driver.find_element_by_xpath(
-        '//*[@id="header"]/div[2]/div/div/form/div[1]/div[1]/div/span/input'
-    ).send_keys(username)
-    driver.find_element_by_xpath(
-        '//*[@id="header"]/div[2]/div/div/form/div[1]/div[2]/div/span/input'
-    ).clear()
-    driver.find_element_by_xpath(
-        '//*[@id="header"]/div[2]/div/div/form/div[1]/div[2]/div/span/input'
-    ).send_keys(password)
+    # driver.find_element(By.XPATH,
+    #     '//*[@id="header"]/div[2]/div/div/div[2]/a'
+    # ).click()
+    # driver.find_element(By.XPATH,
+    #     # '//*[@id="header"]/div[2]/div/div/form/div[1]/div[1]/div/span/input'
+    #     '//*[@id="header"]/div[2]/div/div/div[1]/form/div[1]/div[1]/div/span/input'
+        
+    # ).clear()
+    # driver.find_element(By.XPATH,
+    #     # '//*[@id="header"]/div[2]/div/div/form/div[1]/div[1]/div/span/input'
+    #     '//*[@id="header"]/div[2]/div/div/div[1]/form/div[1]/div[1]/div/span/input'
+    # ).send_keys(username)
+    # driver.find_element(By.XPATH,
+    #     # '//*[@id="header"]/div[2]/div/div/form/div[1]/div[2]/div/span/input'
+    #    '//*[@id="header"]/div[2]/div/div/div[1]/form/div[1]/div[2]/div/span/input'
+    # ).clear()
+    # driver.find_element(By.XPATH,
+    #     # '//*[@id="header"]/div[2]/div/div/form/div[1]/div[2]/div/span/input'
+    #    '//*[@id="header"]/div[2]/div/div/div[1]/form/div[1]/div[2]/div/span/input'
+    # ).send_keys(password)
 
-    time.sleep(1)
-    # 自动点击登录按钮进行登录
-    driver.find_element_by_xpath(
-        '//*[@id="header"]/div[2]/div/div/form/div[4]/a'
-    ).click()
+    # time.sleep(1)
+    # # 自动点击登录按钮进行登录
+    # driver.find_element(By.XPATH,
+    #     # '//*[@id="header"]/div[2]/div/div/form/div[4]/a'
+    #     '//*[@id="header"]/div[2]/div/div/div[1]/form/div[4]/a'
+    # ).click()
     # 拿手机扫二维码！
-    time.sleep(15)
-
-
-def open_link(nickname):
-    # 进入新建图文素材
-    driver.find_element_by_xpath(
-        '//*[@id="menuBar"]/li[4]/ul/li[3]/a/span/span'
-    ).click()
-    driver.find_element_by_xpath(
-        '//*[@id="js_main"]/div[3]/div[1]/div[2]/div[2]/div/a[1]'
-    ).click()
     time.sleep(10)
+
+
+def open_link(nickname, start_page=1):
+    # 写新的图文
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[3]/div[2]/div/div[2]').click()
 
     # 切换到新窗口
     for handle in driver.window_handles:
         if handle != driver.current_window_handle:
-            driver.switch_to_window(handle)
+            driver.switch_to.window(handle)
+           
 
     # 点击超链接
-    driver.find_element_by_xpath('//*[@id="edui23_body"]/div').click()
-    time.sleep(3)
-    # 点击查找文章
-    driver.find_element_by_xpath('//*[@id="myform"]/div[3]/div[1]/div/label[2]').click()
+    driver.find_element(By.XPATH,'//*[@id="js_editor_insertlink"]').click()
+    time.sleep(5)
+    # 选择其他公众号
+    driver.find_element(By.XPATH,'//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[4]/div/div/p/div/button').click()
     # 输入公众号名称
-    driver.find_element_by_xpath(
-        '//*[@id="myform"]/div[3]/div[3]/div[1]/div/span[1]/input'
-    ).clear()
-    driver.find_element_by_xpath(
-        '//*[@id="myform"]/div[3]/div[3]/div[1]/div/span[1]/input'
-    ).send_keys(nickname)
-    # 点击搜索
-    driver.find_element_by_xpath(
-        '//*[@id="myform"]/div[3]/div[3]/div[1]/div/span[1]/a[2]'
-    ).click()
-    time.sleep(3)
+    input_name = driver.find_element(By.XPATH,
+        '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[4]/div/div/div/div/div[1]/span/input'
+    )
+    input_name.clear()
+    input_name.send_keys(nickname)
+    input_name.send_keys(Keys.ENTER)
+    time.sleep(1)
     # 点击第一个公众号
-    driver.find_element_by_xpath(
-        '//*[@id="myform"]/div[3]/div[3]/div[2]/div/div[1]/div/div[1]/div[3]/p[2]'
+    driver.find_element(By.XPATH,
+        '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[4]/div/div/div/div[2]/ul/li[1]/div[1]'
     ).click()
-    time.sleep(3)
+    time.sleep(1)
+
+    # pages
+    total_pages = driver.find_element(By.XPATH, 
+                                        '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[1]/span/label[2]').text
+    current_page = driver.find_element(By.XPATH, 
+                                        '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[1]/span/label[1]').text
+    total_pages = int(total_pages)
+    current_page = int(current_page)
+    ic(total_pages, current_page)
+    
+    if start_page > current_page:
+        current_page = start_page
+    ic(current_page)
+    # 跳转到第几页
+    driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/input').send_keys(Keys.END, Keys.BACK_SPACE)
+    driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/input').send_keys(current_page)
+    driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/a').click()
+    time.sleep(2)
+    # 开始采集
+    url_list = []
+    
+    while True:
+        try:
+            # 查看有几篇文章
+            parent = driver.find_element(By.XPATH,
+                                        '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[2]/div/div')
+            children = parent.find_elements(By.CLASS_NAME, "inner_link_article_item")
+            count = len(children)
+            ic(count)
+            if count <= 0:
+                break
+            
+            for i in range(count):
+                href = driver.find_element(By.XPATH,
+                    '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[2]/div/div/label[{}]/span[2]/a'.format(i+1)
+                    )
+                url = href.get_attribute("href")
+                ic(url)
+                url_list.append(url)
+            
+            if current_page >= total_pages:
+                # 保存csv
+                data_file = "data_p{}.csv".format(current_page)
+                ic(data_file)
+                with open(data_file, "a") as f:
+                    f.write("\n".join(url_list))
+                break
+            
+            # 点击下一页 
+            current_page += 1
+            ic(current_page)
+            driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/input').send_keys(Keys.END, Keys.BACK_SPACE)
+            driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/input').send_keys(current_page) 
+            driver.find_element(By.XPATH, '//*[@id="vue_app"]/div[2]/div[1]/div/div[2]/div[2]/form[1]/div[5]/div/div/div[3]/span[2]/a').click()
+            
+            time.sleep(3+current_page%5)
+        except:
+            # 保存csv
+            data_file = "data_p{}.csv".format(current_page)
+            ic(data_file)
+            with open(data_file, "a") as f:
+                f.write("\n".join(url_list))
+            break
 
 
 def get_url_title(html):
@@ -92,35 +158,9 @@ def get_url_title(html):
 
 
 # 用webdriver启动谷歌浏览器
-driver = webdriver.Chrome(executable_path=chromedriver_path)
+driver = webdriver.Chrome()
 
-nickname = ""  # 公众号名称
-username = ""  # 账号
-password = ""  # 密码
-login(username, password)
-open_link(nickname)
-page_num = int(
-    driver.find_elements_by_class_name("page_num")[-1].text.split("/")[-1].lstrip()
-)
-
-# 点击下一页
-url_title_lst = get_url_title(driver.page_source)
-
-for _ in range(1, page_num):
-    try:
-        pagination = driver.find_elements_by_class_name("pagination")[1]
-        pagination.find_elements_by_tag_name("a")[2].click()
-        time.sleep(5)
-        url_title_lst += get_url_title(driver.page_source)
-    except:
-        # 保存
-        with open("data.pickle", "wb") as f:
-            pickle.dump(url_title_lst, f)
-        print("第{}页失败".format(_))
-        break
-
-with open("data2.pickle", "wb") as f:
-    pickle.dump(data, f)
-# 读取
-with open("data.pickle", "rb") as f:
-    b = pickle.load(f)
+nickname = "L先生说"  # 公众号名称
+start_page = 111  # 开始页码
+ic(login())
+ic(open_link(nickname, start_page))
